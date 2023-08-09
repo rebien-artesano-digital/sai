f25c8c4ef947a0e49226227a115a413936d09172
caarevalo <caarevalo@cognox.com>
2023-08-09 14:33:26 -0500

    fix: Se soluciona bug en las vistas

--- src/providers/AssistentServiceProvider.php
+++ src/providers/AssistentServiceProvider.php
@@ -19,8 +19,8 @@ class AssistentServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../../config/sai.php' => config_path('sai.php'),
             __DIR__.'/../../app/Services/SaiServices.php' => app_path('Services/SaiServices.php'),
-            __DIR__.'/../../resources/views/Layouts/saiAdmin.blade.php'=> resource_path('views/Layouts/saiAdmin.blade.php'),
-            __DIR__.'/../../resources/views/saiup.blade.php'=> resource_path('views/saiup.blade.php'),
+            __DIR__.'/../../resources/views/layouts/assistent.blade.php'=> resource_path('views/layouts/assistent.blade.php'),
+            __DIR__.'/../../resources/views/assistent/assistent.blade.php'=> resource_path('views/assistent/assistent.blade.php'),
             __DIR__.'/../../app/Directives/Methods'=> app_path('Directives/Methods'),
             __DIR__.'/../../app/Directives/Helpers'=> app_path('Directives/Helpers'),
             __DIR__.'/../storage/images'=> storage_path('app/public/images'),
